ee3bf3c958545b533607e574bef30f4ef9bd68fe
Razvan Cranganu <razvan.cranganu@gmail.com>
2021-03-04 17:09:38 +0000

    Added the S3 module

--- sandbox/main.tf
+++ sandbox/main.tf
@@ -1,5 +1,15 @@
+# Create the network
 module "network" {
   source      = "../modules/network"
   region      = var.region
   environment = var.environment
 }
+
+# Create the s3 buckets
+module "s3_bucket" {
+  source      = "../modules/s3_bucket"
+  environment = var.environment
+}
+
+
+
