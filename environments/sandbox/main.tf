d0caab3c88cb844295daddd4a0b031a202e727fc
Razvan Cranganu <razvan.cranganu@gmail.com>
2021-03-05 11:24:26 +0000

    Moved sandbox to enviroments

--- environments/sandbox/main.tf
+++ environments/sandbox/main.tf
@@ -1,25 +1,25 @@
 # Create the network
 module "network" {
-  source      = "../modules/network"
+  source      = "../../modules/network"
   region      = var.region
   environment = var.environment
 }
 
 module "security" {
-  source                   = "../modules/security"
+  source                   = "../../modules/security"
   wordpress_vpc_id         = module.network.vpc_id
   wordpress_environment_id = var.environment
 }
 
 # Create the s3 buckets
 module "s3_bucket" {
-  source      = "../modules/s3_bucket"
+  source      = "../../modules/s3_bucket"
   environment = var.environment
 }
 
 # Create the Bastion instance
 module "bastion" {
-  source             = "../modules/bastion"
+  source             = "../../modules/bastion"
   region             = var.region
   environment        = var.environment
   bastion_baseami    = var.bastion_baseami
